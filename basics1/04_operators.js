dd1e3f05f719a6004d6155b51f16b0e166883df5
Vanshika <138101561+Karma-is-here@users.noreply.github.com>
2024-04-07 16:48:43 +0000

    Operators

---
+++ basics1/04_operators.js
@@ -0,0 +1,18 @@
+// node basics1/operators.js
+/*.......................ARITHMETIC OPERATORS....................*/
+let a=1;
+//console.log(1+1);
+//regular operators
+
+//Tricky Parts
+console.log(1+"2");
+console.log("1"+2);
+
+//every number before string number is treated as a number
+console.log(1+2+"3");
+//as the string is identified in the system, treats everything as string
+console.log("1"+2+3);
+
+/*.......................COMPARISON OPERATORS....................*/
+console.log(2>4);
+
